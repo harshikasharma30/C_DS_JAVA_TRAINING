5795e02ad2fc03ee86874c6db6b3446f04237acd
harshikasharma30 <harshikasharma30@gmail.com>
2024-07-22 15:30:26 +0530

    Add Two Number With Different Data Types In Java

---
+++ Sum.java
@@ -0,0 +1,7 @@
+class AddTwoNumberWithDifferentDataTypes {
+    public static void main(String[] args) {
+         int a = 5;
+         float b = 7.5f;
+         System.out.println("Sum :"+(a+b));
+    }
+}
