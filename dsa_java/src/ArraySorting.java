454df489b2d5d05a3d21b62278861c88030cd250
harshikasharma30 <107365507+harshikasharma30@users.noreply.github.com>
2024-07-25 12:43:06 +0530

    Update ArraySorting.java

--- dsa_java/src/ArraySorting.java
+++ dsa_java/src/ArraySorting.java
@@ -1,5 +1,31 @@
 public class ArraySorting {
     public static void main(String[] args) {
-        int[] number = { 1,7,3,11,24,9 };
+        int[] arr = {1, 7, 3, 11, 24, 9};
+        int temp = 0;
+        System.out.println("Sorting Array in Ascending Order: ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int tmp = 0;
+                if (arr[i] > arr[j]) {
+                    tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+            System.out.println(arr[i]);
+        }
+        System.out.println("Sorting Array in Descending Order: ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i <= arr.length - 1; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
